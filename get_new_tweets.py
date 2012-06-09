7aa9eea437ac74a9cf32d563ef6f15b75913b99d
Adam Matan <adam@matan.name>
2012-06-10 01:38:14 +0300

    Querying twitter using twython

--- get_new_tweets.py
+++ get_new_tweets.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python
 
+import twython
+import pickle
+
+api=twython.Twython()
+
+#r=api.search(q="hbotakemymoney", rpp=100, p=5)
+#pickle.dump(r, open('results','wb'))
+#print '\n'.join(dir(api))
+
+r=pickle.load(open('results'))
+for key in r:
+    print "%-20s %s" % (key, r[key])
+    
+for key in r['results']:
+    print key
