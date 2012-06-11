c62f84b8b2155a676c6c53a5ee5e0e310a9dbfa5
Adam Matan <adam@matan.name>
2012-06-11 22:22:38 +0300

    New class wrapper for updating tweets

--- get_new_tweets.py
+++ get_new_tweets.py
@@ -3,15 +3,42 @@
 import twython
 import pickle
 
-api=twython.Twython()
-
-#r=api.search(q="hbotakemymoney", rpp=100, p=5)
-#pickle.dump(r, open('results','wb'))
-#print '\n'.join(dir(api))
-
-r=pickle.load(open('results'))
-for key in r:
-    print "%-20s %s" % (key, r[key])
+class DataGetter(object):
+    """Retrieves data from twitter search API and updates local files."""
+    def __init__(self, debug=False):
+        self._debug = debug
+        self.api    = twython.Twython()
+        self._load_tweets()
+        self._dprint("%d tweets loaded from file." % len(self.tweets))
     
-for key in r['results']:
-    print key
+    def _load_tweets(self):
+        """Returns a pickled file of {tweet_id:tweet}.
+        Falls back to empty dictionary"""
+        try:
+            self._tweets=pickle.load(open('tweets'))
+        except IOError:
+            self._tweets=dict()
+            
+    @property
+    def tweets(self):
+        return self._tweets
+   
+    def _dprint(self, msg):
+        if self._debug:
+            print msg
+            
+    def update_recent_tweets(self):
+        for page in range(1,16):
+            new_tweets=self.api.search(q="#hbotakemymoney", rpp=50, p=1)['results']
+            self._dprint("%d tweets returned from API call." % len(new_tweets))
+            for tweet in new_tweets:
+                if tweet['id'] not in self.tweets:
+                    self._tweets[tweet['id']]=tweet
+        
+    def save_tweets(self):
+        pickle.dump(self.tweets, open('tweets', 'wb'))
+        
+data_getter=DataGetter(debug=True)
+data_getter.update_recent_tweets()
+data_getter.save_tweets()
+print data_getter.tweets
