afbd9c3b8bc5935bae2110ac0aaafefd5ed7e29f
Adam Matan <adamatan@yahoo-inc.com>
2012-06-12 08:08:47 +0300

    Regex to pull numbers from tweets

--- get_new_tweets.py
+++ get_new_tweets.py
@@ -2,6 +2,7 @@
 
 import twython
 import pickle
+import re
 
 class DataGetter(object):
     """Retrieves data from twitter search API and updates local files."""
@@ -26,19 +27,62 @@ class DataGetter(object):
     def _dprint(self, msg):
         if self._debug:
             print msg
-            
+ 
+    @property
+    def amounts_filtered(self):
+        return [a for a in self.amounts if 0.5 <= a <= 50]
+ 
+    @property
+    def amounts(self):
+        texts=self.texts
+        return sorted([texts[key][1] for key in texts])
+    
+    @property
+    def texts(self):
+        tweet_re=re.compile('^I would pay\s\$(.*)\sa month for a standalone @HBOGO subscription')
+        result=dict()
+        for key in self.tweets:
+            text=self.tweets[key]['text']
+            #print key, text, tweet_re.search(text)
+            if tweet_re.search(text):
+                try:
+                    amount=float(tweet_re.findall(text)[0])
+                    result[key]=tuple([text, amount])
+                except ValueError:
+                    pass
+        return result
+    
     def update_recent_tweets(self):
-        for page in range(1,16):
-            new_tweets=self.api.search(q="#hbotakemymoney", rpp=50, p=1)['results']
-            self._dprint("%d tweets returned from API call." % len(new_tweets))
+        page=1
+        while True:
+            num_existing_tweets=len(self.tweets)
+            new_tweets=self.api.search(q="#takemymoneyHBO", rpp=100, page=page, result_type="recent")['results']
+            self._dprint("%d tweets returned from API call." % len(new_tweets)), 
             for tweet in new_tweets:
                 if tweet['id'] not in self.tweets:
                     self._tweets[tweet['id']]=tweet
+            num_new_tweets=len(self.tweets)-num_existing_tweets
+            if num_new_tweets==0:
+                print "No new tweets."
+                break
+            else:
+                print "%d new tweets." % num_new_tweets
+                self.save_tweets()
+                page+=1
         
     def save_tweets(self):
         pickle.dump(self.tweets, open('tweets', 'wb'))
         
 data_getter=DataGetter(debug=True)
-data_getter.update_recent_tweets()
-data_getter.save_tweets()
-print data_getter.tweets
+#data_getter.update_recent_tweets()
+#data_getter.save_tweets()
+#print len(data_getter.tweets)
+#print data_getter.texts
+amounts=data_getter.amounts_filtered
+amounts_size=len(amounts)
+print amounts
+print sum(amounts)/amounts_size
+percentile=20
+for i in range(1,percentile):
+    loc=i*amounts_size/percentile
+    print loc, amounts[loc]
